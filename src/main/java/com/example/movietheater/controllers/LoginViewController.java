7cb9b2f1c1ecf6c900976ca9aefa576c0f1410b1
Willow <williamepduff@gmail.com>
2025-12-01 15:22:11 -0500

    Added view controller for login view

---
+++ src/main/java/com/example/movietheater/controllers/LoginViewController.java
@@ -0,0 +1,26 @@
+package com.example.movietheater.controllers;
+
+import javafx.fxml.FXML;
+
+public class LoginViewController
+{
+    @FXML
+    private TextField nameTextPrompt;
+
+    @FXML
+    private TextField passwordTextPrompt;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button managerLoginButton;
+
+    @FXML
+    private Button signupButton;
+
+
+}
